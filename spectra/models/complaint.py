d006da0aa82b559569eadd622b1bc6bd1d9bf7e4
Markous Soliman <msolima002@citymail.cuny.edu>
2015-05-06 12:05:07 -0400

    i tried to add complaint model

--- spectra/models/complaint.py
+++ spectra/models/complaint.py
@@ -1,17 +1,16 @@
 from spectra.models import db
 
-class Invitation(db.Model):
-    __tablename__ = 'invitations'
+class Complaint(db.Model):
+    __tablename__ = 'complaints'
 
     id = db.Column(db.Integer, primary_key=True)
-    salesperson_id = db.Column(db.Integer)
-    invite_code = db.Column(db.String(255), unique=True)
-    email = db.Column(db.String(255))
     user_id = db.Column(db.Integer)
+    complainer_id = db.Column(db.Integer)
+    details = db.Column(db.String)
     date = db.Column(db.DateTime)
 
-    def __init__(self, invite_code):
-		self.invite_code = invite_code
+    def __init__(self, details):
+		self.details = details
 
     def __repr__(self):
-        return '<Invitation %r>' % self.invite_code
+        return '<Complaint %r>' % self.details
