8763adc3bcc0a30284c89fccea64e6a95d2d7f91
unknown <echan3960>
2015-05-08 17:30:57 -0400

    checkout

---
+++ spectra/models/order.py
@@ -0,0 +1,25 @@
+from spectra.models import db
+
+class Order(db.Model):
+	__tablename__ = 'orders'
+
+	id = db.Column(db.Integer, primary_key=True)
+	client_id = db.Column(db.Integer)
+	salesperson_id = db.Column(db.Integer)    
+	date = db.Column(db.DateTime)
+	date_approved = db.Column(db.DateTime)
+	date_rejected = db.Column(db.DateTime)
+	total = db.Column(db.Integer) 
+	discount = db.Column(db.Integer) 
+
+	def __init__(self, client_id, salesperson_id, date, date_approved, date_rejected, total, discount):
+		self.client_id = client_id
+		self.salesperson_id = salesperson_id
+		self.date = date
+		self.date_approved = date_approved
+		self.date_rejected = date_rejected
+		self.total = total
+		self.discount = discount
+
+	def __repr__(self):
+		return '<Order %r>' % self.id
